978062ff397464b9bae2d684aad919db9658447f
Aziz M. Bookwala <aziz.mansur@gmail.com>
2013-07-17 12:55:45 +0400

    Updating templatetags for staticfiles compatibility

--- filer/templatetags/filer_admin_tags.py
+++ filer/templatetags/filer_admin_tags.py
@@ -3,7 +3,14 @@ from django.conf import settings
 from django.template import Library
 from distutils.version import LooseVersion
 
+if "django.contrib.staticfiles" in settings.INSTALLED_APPS:
+    from django.contrib.staticfiles.templatetags.staticfiles import static
+else:
+    static = lambda path: "%s%s" % (settings.STATIC_URL, path)
+
+
 register = Library()
+
 
 def filer_actions(context):
     """
@@ -18,13 +25,13 @@ filer_actions = register.inclusion_tag("admin/filer/actions.html", takes_context
 # Shamelessly taken from django-cms
 # This will go away when django < 1.4 compatibility will be dropped
 if LooseVersion(django.get_version()) < LooseVersion('1.4'):
-    ADMIN_ICON_BASE = "%sadmin/img/admin/" % settings.STATIC_URL
-    ADMIN_CSS_BASE = "%sadmin/css/" % settings.STATIC_URL
-    ADMIN_JS_BASE = "%sadmin/js/" % settings.STATIC_URL
+    ADMIN_ICON_BASE = static("admin/img/admin/")
+    ADMIN_CSS_BASE = static("admin/css/")
+    ADMIN_JS_BASE = static("admin/js/")
 else:
-    ADMIN_ICON_BASE = "%sadmin/img/" % settings.STATIC_URL
-    ADMIN_CSS_BASE = "%sadmin/css/" % settings.STATIC_URL
-    ADMIN_JS_BASE = "%sadmin/js/" % settings.STATIC_URL
+    ADMIN_ICON_BASE = static("admin/img/")
+    ADMIN_CSS_BASE = static("admin/css/")
+    ADMIN_JS_BASE = static("admin/js/")
 
 @register.simple_tag
 def admin_icon_base():
